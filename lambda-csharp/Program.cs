74779db2f03ebf615716b2239e31541fcde01517
Jhonatas Jardim <jhonatasjardim@hotmail.com>
2020-12-10 14:23:18 -0300

    Inserindo bloco try

--- lambda-csharp/Program.cs
+++ lambda-csharp/Program.cs
@@ -16,32 +16,40 @@ namespace Course
 
             List<Employee> list = new List<Employee>();
 
-            using (StreamReader sr = File.OpenText(path))
+            try
             {
-                while (!sr.EndOfStream)
+                using (StreamReader sr = File.OpenText(path))
                 {
-                    string[] fields = sr.ReadLine().Split(',');
-                    string name = fields[0];
-                    string email = fields[1];
-                    double salary = double.Parse(fields[2], CultureInfo.InvariantCulture);
-                    list.Add(new Employee(name, email, salary));
+                    while (!sr.EndOfStream)
+                    {
+                        string[] fields = sr.ReadLine().Split(',');
+                        string name = fields[0];
+                        string email = fields[1];
+                        double salary = double.Parse(fields[2], CultureInfo.InvariantCulture);
+                        list.Add(new Employee(name, email, salary));
+                    }
                 }
+
+                Console.Write("Enter salary: ");
+                double moreSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.WriteLine("Email of people whose salary is more than " + moreSalary.ToString("F2", CultureInfo.InvariantCulture) + ":");
+
+                var emails = list.Where(p => p.Salary > moreSalary).OrderBy(p => p.Email).Select(p => p.Email);
+
+                foreach (string email in emails)
+                {
+                    Console.WriteLine(email);
+                }
+
+                var sum = list.Where(p => p.Name[0] == 'M').Sum(p => p.Salary);
+
+                Console.WriteLine("Sum of salary of people whose name starts with 'M': " + sum.ToString("F2", CultureInfo.InvariantCulture));
             }
-
-            Console.Write("Enter salary: ");
-            double moreSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            Console.WriteLine("Email of people whose salary is more than " + moreSalary.ToString("F2", CultureInfo.InvariantCulture) + ":");
-
-            var emails = list.Where(p => p.Salary > moreSalary).OrderBy(p => p.Email).Select(p => p.Email);
-
-            foreach (string email in emails)
+            catch (IOException e)
             {
-                Console.WriteLine(email);
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
             }
-
-            var sum = list.Where(p => p.Name[0] == 'M').Sum(p => p.Salary);
-
-            Console.WriteLine("Sum of salary of people whose name starts with 'M': " + sum.ToString("F2", CultureInfo.InvariantCulture));
         }
     }
 }
