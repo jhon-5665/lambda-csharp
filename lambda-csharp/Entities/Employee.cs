066ecb06ed2ed1db0b51c02931049b46f3d0ec7f
Jhonatas Jardim <jhonatasjardim@hotmail.com>
2020-12-10 14:08:32 -0300

    Projeto finalizado

--- lambda-csharp/Entities/Employee.cs
+++ lambda-csharp/Entities/Employee.cs
@@ -1,10 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace lambda_csharp.Entities
+﻿namespace Course.Entities
 {
     class Employee
     {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(string name, string email, double salary)
+        {
+            Name = name;
+            Email = email;
+            Salary = salary;
+        }
     }
 }
--- lambda-csharp/Program.cs
+++ lambda-csharp/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using Course.Entities;
+using System.IO;
+using System.Linq;
 
 namespace Course
 {
@@ -6,7 +11,37 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter full file path: ");
+            string path = Console.ReadLine();
+
+            List<Employee> list = new List<Employee>();
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] fields = sr.ReadLine().Split(',');
+                    string name = fields[0];
+                    string email = fields[1];
+                    double salary = double.Parse(fields[2], CultureInfo.InvariantCulture);
+                    list.Add(new Employee(name, email, salary));
+                }
+            }
+
+            Console.Write("Enter salary: ");
+            double moreSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Email of people whose salary is more than " + moreSalary.ToString("F2", CultureInfo.InvariantCulture) + ":");
+
+            var emails = list.Where(p => p.Salary > moreSalary).OrderBy(p => p.Email).Select(p => p.Email);
+
+            foreach (string email in emails)
+            {
+                Console.WriteLine(email);
+            }
+
+            var sum = list.Where(p => p.Name[0] == 'M').Sum(p => p.Salary);
+
+            Console.WriteLine("Sum of salary of people whose name starts with 'M': " + sum.ToString("F2", CultureInfo.InvariantCulture));
         }
     }
 }
